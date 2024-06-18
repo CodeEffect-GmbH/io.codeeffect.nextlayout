71131cb2c0614e1450a5c5364d226535402d2f7d
Dino Fejzagić <dino.f@live.de>
2024-06-18 22:54:12 +0200

    Make footer customizable

--- cards/SimpleCard.tsx
+++ cards/SimpleCard.tsx
@@ -2,22 +2,23 @@ import { FunctionComponent, ReactElement } from "react";
 import React from "react";
 import Title from "../typography/title";
 import Text from "../typography/text";
-import LinkEx from "../link-ex";
 
 export interface SimpleCardProps {
     title: string;
     description: string | ReactElement;
+    footer?: ReactElement;
 }
 
 const SimpleCard: FunctionComponent<SimpleCardProps> = ({
     title,
-    description
+    description,
+    footer = null
 }) => {
     return (
         <div className="bg-white rounded-lg p-4 md:p-8 flex flex-col gap-4">
             <Title compact level={5}>{title}</Title>
             {React.isValidElement(description) ? description : <Text compact>{description}</Text>}
-            <LinkEx href="/services" title="Mehr erfahren">Mehr erfahren</LinkEx>
+            {footer && footer}
         </div>
     );
 }
