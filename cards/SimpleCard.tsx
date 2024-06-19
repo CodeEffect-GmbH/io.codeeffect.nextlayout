31eb281b2b9b337f7dc41549dedae6f6b797863e
Dino Fejzagić <dino.f@live.de>
2024-06-19 11:50:11 +0200

    Grow card body

--- cards/SimpleCard.tsx
+++ cards/SimpleCard.tsx
@@ -17,7 +17,9 @@ const SimpleCard: FunctionComponent<SimpleCardProps> = ({
     return (
         <div className="bg-white rounded-lg p-4 md:p-8 flex flex-col gap-4">
             <Title compact level={5}>{title}</Title>
-            {React.isValidElement(description) ? description : <Text compact>{description}</Text>}
+            <div className="flex-1">
+                {React.isValidElement(description) ? description : <Text compact>{description}</Text>}
+            </div>
             {footer && footer}
         </div>
     );
