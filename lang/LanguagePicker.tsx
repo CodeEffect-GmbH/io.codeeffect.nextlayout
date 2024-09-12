9831ec019165bdc3e57d45ef8dc16faedf4a4a4c
Dino Fejzagić <dino.f@live.de>
2024-09-12 12:05:06 +0200

    Redesign language picker

--- lang/LanguagePicker.tsx
+++ lang/LanguagePicker.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ChangeEvent, FunctionComponent, ReactElement } from "react";
+import { ChangeEvent, FunctionComponent, ReactElement, useState } from "react";
 import { LangProps } from "./lang.model";
 import Text from "../typography/text";
 import { useRouter, usePathname } from 'next/navigation';
@@ -21,29 +21,32 @@ const LanguagePicker: FunctionComponent<LanguagePickerProps> = ({
 }) => {
     const router = useRouter();
     const pathname = usePathname();
+    const [selectedLanguage, setSelectedLanguage] = useState(languages[0]);
+    const [isOpen, setIsOpen] = useState(true);
 
-    const handleChange = (event: ChangeEvent<HTMLSelectElement>): void => {
-        const selectedLanguage = event.target.value;
+    const handleLanguageChange = (language: Language): void => {
+        setSelectedLanguage(language);
         const languagePattern = new RegExp(`^/(${languages.map(l => l.code).join('|')})`);
         const pathnameWithoutLocale = pathname.replace(languagePattern, '');
-        router.replace(`/${selectedLanguage}${pathnameWithoutLocale}`);
+        router.replace(`/${selectedLanguage.code}${pathnameWithoutLocale}`);
     };
 
     return (
-        <div>
-            <select
-                value={lang}
-                className="p-2 border-0"
-                onChange={handleChange}>
-                {languages.map((language) => (
-                    <option key={language.code} value={language.code}>
-                        <div className="flex">
-                            <div>{language.flag}</div>
-                            <Text>{language.name}</Text>
-                        </div>
-                    </option>
-                ))}
-            </select>
+        <div className="relative inline-block cursor-pointer">
+            <div className="flex items-center" onClick={() => setIsOpen(!isOpen)}>
+                <div>{selectedLanguage.flag}</div>
+            </div>
+
+            {isOpen &&
+                <ul className="absolute flex flex-col bg-white p-2 rounded-lg mt-4 list-none gap-2 right-0">
+                    {languages.map((language) => (
+                        <li className="cursor-pointer flex items-center" key={language.code} onClick={() => handleLanguageChange(language)}>
+                            {language.flag}
+                            <span>{language.name}</span>
+                        </li>
+                    ))}
+                </ul>
+            }
         </div>
     );
 }
