4320cd0f9b55fc4836009a18d199f3fcd1fb6ed7
Dino Fejzagić <dino.f@live.de>
2024-10-10 09:21:37 +0200

    Add stars to testimonial card (optional) and make image optional

--- cards/TestimonialCard.tsx
+++ cards/TestimonialCard.tsx
@@ -5,23 +5,27 @@ import Text from "../typography/text";
 import Image from "next/image";
 
 export interface TestimonialCardProps {
-    imageSrc: string;
+    imageSrc?: string;
     name: string;
     position: string;
     text: string;
+    showStars?: boolean;
 }
 
 const TestimonialCard: FunctionComponent<TestimonialCardProps> = ({
     imageSrc,
     name,
     position,
-    text
+    text,
+    showStars = false
 }) => {
     return (
-        <div className="bg-white rounded-lg p-4 md:p-8 flex flex-col gap-4">
-            <div className="relative">
-                <Image className="rounded-full" src={imageSrc} alt={name} width={64} height={64} />
-            </div>
+        <div className="relative bg-white rounded-lg p-4 md:p-8 flex flex-col gap-4">
+            {imageSrc &&
+                <div className="relative">
+                    <Image className="rounded-full" src={imageSrc} alt={name} width={64} height={64} />
+                </div>
+            }
             <div>
                 <Title compact level={5}>{name}</Title>
                 <Text level={2}>{position}</Text>
@@ -29,6 +33,26 @@ const TestimonialCard: FunctionComponent<TestimonialCardProps> = ({
             <div className="flex-1">
                 <Text compact>{text}</Text>
             </div>
+
+            {showStars &&
+                <div className="absolute top-0 right-0 p-4 md:p-8 flex text-amber-300">
+                    <div className="w-6 h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><title>{name}</title><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" /></svg>
+                    </div>
+                    <div className="w-6 h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><title>{name}</title><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" /></svg>
+                    </div>
+                    <div className="w-6 h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><title>{name}</title><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" /></svg>
+                    </div>
+                    <div className="w-6 h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><title>{name}</title><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" /></svg>
+                    </div>
+                    <div className="w-6 h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><title>{name}</title><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" /></svg>
+                    </div>
+                </div>
+            }
         </div>
     );
 }
