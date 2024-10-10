4e683e6aaa752aa4662ee3b1189cd3c2e94070d3
Dino Fejzagić <dino.f@live.de>
2024-10-10 12:14:31 +0200

    Reorganize cards

--- cards/SimpleCard.tsx
+++ cards/SimpleCard.tsx
@@ -4,21 +4,21 @@ import Title from "../typography/title";
 import Text from "../typography/text";
 
 export interface SimpleCardProps {
-    title: string;
-    description: string | ReactElement;
+    title: string | ReactElement;
+    body: string | ReactElement;
     footer?: ReactElement;
 }
 
 const SimpleCard: FunctionComponent<SimpleCardProps> = ({
     title,
-    description,
+    body,
     footer = null
 }) => {
     return (
         <div className="bg-white rounded-lg p-4 md:p-8 flex flex-col gap-4">
-            <Title compact level={5}>{title}</Title>
+            {React.isValidElement(title) ? title : <Title compact level={5}>{title}</Title>}
             <div className="flex-1">
-                {React.isValidElement(description) ? description : <Text compact>{description}</Text>}
+                {React.isValidElement(body) ? body : <Text compact>{body}</Text>}
             </div>
             {footer && footer}
         </div>
