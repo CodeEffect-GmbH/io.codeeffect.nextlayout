483c5171a7249e247fbdf5b6a4f54d0f2b5f4b51
Dino Fejzagić <dino.f@live.de>
2024-11-29 17:34:33 +0100

    Update compact section margin

--- sections/Section.tsx
+++ sections/Section.tsx
@@ -9,7 +9,7 @@ const Section: FunctionComponent<PropsWithChildren<MinimalSectionProps>> = ({
     compact
 }) => {
     return (
-        <div className={`relative flex justify-center px-4 md:px-8 ${compact ? `pt-16 lg:pt-24 ${forceLast ? 'pb-16 lg:pb-24' : 'last:pb-16 lg:last:pb-24'}` : `pt-24 lg:pt-28 ${forceLast ? 'pb-24 lg:pb-28' : 'last:pb-24 lg:last:pb-28'}`} ${className ? className : ''}`}>
+        <div className={`relative flex justify-center px-4 md:px-8 ${compact ? `pt-8 lg:pt-12 ${forceLast ? 'pb-8 lg:pb-12' : 'last:pb-8 lg:last:pb-12'}` : `pt-24 lg:pt-28 ${forceLast ? 'pb-24 lg:pb-28' : 'last:pb-24 lg:last:pb-28'}`} ${className ? className : ''}`}>
             {anchorId && <a className="absolute top-24" id={anchorId} />}
             {children}
         </div>
