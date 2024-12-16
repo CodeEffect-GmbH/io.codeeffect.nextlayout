1fe56bb0821eb3af8eb4b7520bca08f584debae1
Dino Fejzagić <dino.f@live.de>
2024-12-16 12:49:48 +0100

    Remove custom styles from drawer

--- drawer.tsx
+++ drawer.tsx
@@ -1,16 +1,18 @@
 import { FunctionComponent, PropsWithChildren } from "react";
+import { ClassNameProps } from "./components.model";
 
-export interface DrawerProps {
+export interface DrawerProps extends ClassNameProps {
     isOpen: boolean;
 }
 
 const Drawer: FunctionComponent<PropsWithChildren<DrawerProps>> = ({
     isOpen,
+    className,
     children
 }) => {
     return (
         <div>
-            <div className={`shadow-md fixed top-0 right-0 bottom-0 w-3/5 lg:w-1/4 h-full transition-transform ease-in-out bg-white px-4 md:px-8 p-32 lg:pt-44 ${isOpen ? '' : 'translate-x-full'}`}>
+            <div className={`fixed top-0 right-0 bottom-0 w-3/5 lg:w-1/4 h-full transition-transform ease-in-out ${isOpen ? '' : 'translate-x-full'} ${className}`}>
                 {children}
             </div>
         </div>
