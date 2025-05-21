cd5e642c04bd02e49bb9ef90358add571d2769d6
Dino Fejzagić <dino.f@live.de>
2025-05-21 13:48:21 +0200

    Formatting

--- cards/PricingCard.tsx
+++ cards/PricingCard.tsx
@@ -1,67 +1,82 @@
-import { FunctionComponent, ReactElement } from "react";
-import Title from "../typography/title";
-import Text from "../typography/text";
-import BaseCard from "./BaseCard";
-import { ClassNameProps } from "../components.model";
+import { FunctionComponent, ReactElement } from 'react';
+import Title from '../typography/title';
+import Text from '../typography/text';
+import BaseCard from './BaseCard';
+import { ClassNameProps } from '../components.model';
 
 export interface PricingCardProps extends ClassNameProps {
-    headerClassName?: string;
-    bodyClassName?: string;
-    footerClassName?: string;
-    tierName: string;
-    tierPrice: string;
-    tierPriceNote?: string;
-    action: ReactElement;
-    features: TierFeature[];
+  headerClassName?: string;
+  bodyClassName?: string;
+  footerClassName?: string;
+  tierName: string;
+  tierPrice: string;
+  tierPriceNote?: string;
+  action: ReactElement;
+  features: TierFeature[];
 }
 
 export interface TierFeature {
-    icon: ReactElement;
-    text: string;
-    textClassName?: string;
+  icon: ReactElement;
+  text: string;
+  textClassName?: string;
 }
 
 const PricingCard: FunctionComponent<PricingCardProps> = ({
-    className,
-    headerClassName,
-    bodyClassName,
-    footerClassName,
-    tierName,
-    tierPrice,
-    tierPriceNote,
-    action,
-    features
+  className,
+  headerClassName,
+  bodyClassName,
+  footerClassName,
+  tierName,
+  tierPrice,
+  tierPriceNote,
+  action,
+  features,
 }) => {
-    return (
-        <BaseCard className={className} title={
-            <div className={`p-4 md:p-8 flex justify-center items-center ${headerClassName}`}>
-                <div className="text-center">
-                    <Title level={5}>{tierName}</Title>
-                    <div>
-                        <Title uppercase level={4}>{tierPrice}</Title>
-                        {tierPriceNote && <Text level={2}>{tierPriceNote}</Text>}
-                    </div>
-                </div>
+  return (
+    <BaseCard
+      className={className}
+      title={
+        <div
+          className={`p-4 md:p-8 flex justify-center items-center ${headerClassName}`}>
+          <div className="text-center">
+            <Title level={5}>{tierName}</Title>
+            <div>
+              <Title
+                uppercase
+                level={4}>
+                {tierPrice}
+              </Title>
+              {tierPriceNote && <Text level={2}>{tierPriceNote}</Text>}
             </div>
-        }
-            body={
-                <div className={bodyClassName}>
-                    <div className="grid grid-cols-1">
-                        {features.map((feature, index) =>
-                            <div key={index} className="px-4 md:px-8 py-4 flex items-center gap-4">
-                                <div>{feature.icon}</div>
-                                <Text compact className={feature.textClassName}>{feature.text}</Text>
-                            </div>
-                        )}
-                    </div>
-                </div>
-            }
-            footer={
-                <div className={`flex justify-center items-center p-4 md:p-8 ${footerClassName}`}>
-                    {action}
-                </div>
-            } />
-    );
-}
+          </div>
+        </div>
+      }
+      body={
+        <div className={bodyClassName}>
+          <div className="grid grid-cols-1 py-4">
+            {features.map((feature, index) => (
+              <div
+                key={index}
+                className="px-4 md:px-8 py-2 flex items-center gap-4">
+                <div>{feature.icon}</div>
+                <Text
+                  compact
+                  className={feature.textClassName}>
+                  {feature.text}
+                </Text>
+              </div>
+            ))}
+          </div>
+        </div>
+      }
+      footer={
+        <div
+          className={`flex justify-center items-center p-4 md:p-8 ${footerClassName}`}>
+          {action}
+        </div>
+      }
+    />
+  );
+};
 
 export default PricingCard;
--- link-button.tsx
+++ link-button.tsx
@@ -1,31 +1,40 @@
-import { FunctionComponent, PropsWithChildren, ReactNode } from "react";
-import Title from "./typography/title";
-import { ClassNameProps } from "./components.model";
-import LinkEx from "./link-ex";
+import { FunctionComponent, PropsWithChildren, ReactNode } from 'react';
+import Title from './typography/title';
+import { ClassNameProps } from './components.model';
+import LinkEx from './link-ex';
 
 export interface LinkButtonProps extends ClassNameProps {
-    icon?: ReactNode;
-    href: string;
-    title: string;
+  icon?: ReactNode;
+  href: string;
+  title: string;
 }
 
 const LinkButton: FunctionComponent<PropsWithChildren<LinkButtonProps>> = ({
-    icon,
-    href,
-    title,
-    className,
-    children
+  icon,
+  href,
+  title,
+  className,
+  children,
 }) => {
-    return (
-        <LinkEx href={href} title={title} className={`flex items-center cursor-pointer p-4 transition-transform hover:drop-shadow-md hover:scale-105 ${className}`}>
-            <div className="flex items-center">
-                {children && <div className={`${icon ? 'mr-4' : ''}`}>
-                    <Title level={5} compact>{children}</Title>
-                </div>}
-                {icon && <div>{icon}</div>}
-            </div>
-        </LinkEx>
-    );
-}
+  return (
+    <LinkEx
+      href={href}
+      title={title}
+      className={`flex items-center cursor-pointer p-4 transition-transform hover:drop-shadow-md hover:scale-105 ${className}`}>
+      <div className="flex items-center">
+        {children && (
+          <div className={`${icon ? 'mr-4' : ''}`}>
+            <Title
+              level={5}
+              compact>
+              {children}
+            </Title>
+          </div>
+        )}
+        {icon && <div>{icon}</div>}
+      </div>
+    </LinkEx>
+  );
+};
 
 export default LinkButton;
