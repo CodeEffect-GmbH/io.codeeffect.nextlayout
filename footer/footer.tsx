07065d012c0cab96c4f2d668bc106848d611ff3f
Dino Fejzagić <dino.f@live.de>
2025-05-21 17:46:45 +0200

    Format compoennt

--- footer/footer.tsx
+++ footer/footer.tsx
@@ -1,48 +1,58 @@
-'use client'
+'use client';
 
-import { FunctionComponent, PropsWithChildren } from "react";
-import MaxWidthContainer from "../sections/max-width-container";
-import CopyrightNotice from "../copyright-notice";
-import { ClassNameProps } from "../components.model";
+import { FunctionComponent, PropsWithChildren } from 'react';
+import MaxWidthContainer from '../sections/max-width-container';
+import CopyrightNotice from '../copyright-notice';
+import { ClassNameProps } from '../components.model';
 
 export interface FooterProps extends ClassNameProps {
-    copyrightNotice?: string;
-    backToTopTitle: string;
-    backToTopClassName?: string;
+  copyrightNotice?: string;
+  backToTopTitle: string;
+  backToTopClassName?: string;
 }
 
 const Footer: FunctionComponent<PropsWithChildren<FooterProps>> = ({
-    copyrightNotice,
-    backToTopTitle,
-    backToTopClassName,
-    className,
-    children
+  copyrightNotice,
+  backToTopTitle,
+  backToTopClassName,
+  className,
+  children,
 }) => {
-    const backToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
-    };
+  const backToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
 
-    return (
-        <div className={`p-4 md:p-8 flex justify-center ${className}`}>
-            <MaxWidthContainer>
-                <div>
-                    {children && <div className="py-8 md:py-12">{children}</div>}
-                    <div className="flex justify-between items-center">
-                        {copyrightNotice && <CopyrightNotice>{copyrightNotice}</CopyrightNotice>}
+  return (
+    <div className={`p-4 md:p-8 flex justify-center ${className}`}>
+      <MaxWidthContainer>
+        <div>
+          {children && <div className="py-8 md:py-12">{children}</div>}
+          <div className="flex justify-between items-center">
+            {copyrightNotice && (
+              <CopyrightNotice>{copyrightNotice}</CopyrightNotice>
+            )}
 
-                        <button className={`hover:scale-105 hover:drop-shadow-md transition-transform ${backToTopClassName}`} onClick={backToTop}>
-                            <div className="relative w-10 h-10">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><title>{backToTopTitle}</title><path d="M15,20H9V12H4.16L12,4.16L19.84,12H15V20Z" /></svg>
-                            </div>
-                        </button>
-                    </div>
-                </div>
-            </MaxWidthContainer>
+            <button
+              className={`hover:scale-105 hover:drop-shadow-md transition-transform ${backToTopClassName}`}
+              onClick={backToTop}>
+              <div className="relative w-10 h-10">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 24 24">
+                  <title>{backToTopTitle}</title>
+                  <path d="M15,20H9V12H4.16L12,4.16L19.84,12H15V20Z" />
+                </svg>
+              </div>
+            </button>
+          </div>
         </div>
-    );
-}
+      </MaxWidthContainer>
+    </div>
+  );
+};
 
 export default Footer;
